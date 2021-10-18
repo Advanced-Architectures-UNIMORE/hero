5908194c0acc15ae9c3dd2a844fe870ece1d8b0a
Gianluca Bellocchi <gbellocchi@badile14.ee.ethz.ch>
2021-10-18 12:36:46 +0200

    Support for both unimore and iis call to vsim

--- hardware/vsim/start_sim.sh
+++ hardware/vsim/start_sim.sh
@@ -4,10 +4,30 @@ set -e
 
 readonly vsim_gui="$1"
 
-if $vsim_gui; then
-    # Run in console-only mode.
-    vsim-10.7b -c -do 'source run.tcl; quit -code $quitCode'
-else
+if [ $UNIMORE -eq 1 ]; then
+if [ $vsim_gui -eq 1 ]; then
     # Run in GUI mode and silence console output.
+    echo "UNIMORE setup - GUI mode"
+    sleep 1s
+    vsim -do 'source run.tcl' &>/dev/null
+else
+    # Run in console-only mode.
+    echo "UNIMORE setup - Console-only mode"
+    sleep 1s
+    vsim -c -do 'source run.tcl; quit -code $quitCode'
+fi
+fi
+
+if [ $IIS -eq 1 ]; then
+if [ $vsim_gui -eq 1 ]; then
+    # Run in GUI mode and silence console output.
+    echo "IIS setup - GUI mode"
+    sleep 1s
     vsim-10.7b -do 'source run.tcl' &>/dev/null
+else
+    # Run in console-only mode.
+    echo "IIS setup - Console-only mode"
+    sleep 1s
+    vsim-10.7b -c -do 'source run.tcl; quit -code $quitCode'
+fi
 fi
