20c54a8b46d6e2136931ad2c908d099471739329
Gianluca Bellocchi <gbellocchi@badile14.ee.ethz.ch>
2021-10-22 20:02:23 +0200

    little draft of new doc. modified various files (still little mods).

--- hardware/src/pulp_cluster_ooc.sv
+++ hardware/src/pulp_cluster_ooc.sv
@@ -171,8 +171,8 @@ module pulp_cluster_ooc (
     .TCDM_SIZE                (pulp_cluster_cfg_pkg::TCDM_SIZE),
     .NB_TCDM_BANKS            (pulp_cluster_cfg_pkg::N_TCDM_BANKS),
     // HWPE
-    .HWPE_PRESENT            (overlay_cluster_hwpe_pkg::HWPE_PRESENT),
-    .NB_HWPE_PORTS           (overlay_cluster_hwpe_pkg::N_HWPE_PORTS),
+    .HWPE_PRESENT             (overlay_cluster_hwpe_pkg::HWPE_PRESENT),
+    .NB_HWPE_PORTS            (overlay_cluster_hwpe_pkg::N_HWPE_PORTS),
     // I$ Parameters
     .NB_CACHE_BANKS           (4),
     .CACHE_SIZE               (pulp_cluster_cfg_pkg::ICACHE_SIZE),
