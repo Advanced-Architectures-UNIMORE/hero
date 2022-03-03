35eab7ef60e8d7ac4ef6910a12b0ffd934be48f3
Gianluca Bellocchi <gbellocchi@badile14.ee.ethz.ch>
2022-03-03 10:39:14 +0100

    further mods to setup-ov-libs. overlay config files are sym-linked to hero-urania/sim as to keep the legacy to old hero-sdk. still, this is a temporary solution.

--- pulp/setup-ov-libs.sh
+++ pulp/setup-ov-libs.sh
@@ -37,19 +37,29 @@ fi
 
 # Device install dir
 install_dir=${PULP_SDK_HOME}/install
-install_cfg_dir=${install_dir}/overlay/${ov_cfg_device}
+install_hero_cfg_dir=${install_dir}/hero
+install_ov_cfg_dir=${install_dir}/overlay/${ov_cfg_device}
 install_inc_dir=${install_dir}/headers/${ov_cfg_device}
 install_lib_dir=${install_dir}/lib/${ov_cfg_device}
 
 # Unlink old symbolic links
+# headers
 if [ -L ${install_dir}/include ]; then
   unlink ${install_dir}/include
 fi
+# libs
 if [ -L ${install_lib_dir}/../${pulp_chip} ]; then
   unlink ${install_lib_dir}/../${pulp_chip}
 fi
 if [ -L ${install_lib_dir}/../hero-sim ]; then
   unlink ${install_lib_dir}/../hero-sim
+fi
+# config files
+if [ -L ${install_hero_cfg_dir}/hero-urania ]; then
+  unlink ${install_hero_cfg_dir}/hero-urania
+fi
+if [ -L ${install_hero_cfg_dir}/hero-sim ]; then
+  unlink ${install_hero_cfg_dir}/hero-sim
 fi
 
 # ------------------------------------------------------------------------------------- #
@@ -92,29 +102,33 @@ ${THIS_DIR}/setup-libprem-pulp.sh "${THIS_DIR}/.."
 # CONFIGURATION FILES
 
 # Remove previous versions of same overlay instance
-if [ -d "${install_cfg_dir}/xilzcu102" ]; then
-    if [ -n "$(ls -A ${install_cfg_dir}/xilzcu102 2>/dev/null)" ]; then
-        rm ${install_cfg_dir}/xilzcu102/*
+if [ -d "${install_ov_cfg_dir}/xilzcu102" ]; then
+    if [ -n "$(ls -A ${install_ov_cfg_dir}/xilzcu102 2>/dev/null)" ]; then
+        rm ${install_ov_cfg_dir}/xilzcu102/*
     fi
 fi
-if [ -d "${install_cfg_dir}/sim" ]; then
-    if [ -n "$(ls -A ${install_cfg_dir}/sim 2>/dev/null)" ]; then
-        rm ${install_cfg_dir}/sim/*
+if [ -d "${install_ov_cfg_dir}/sim" ]; then
+    if [ -n "$(ls -A ${install_ov_cfg_dir}/sim 2>/dev/null)" ]; then
+        rm ${install_ov_cfg_dir}/sim/*
     fi
 fi
 
 # Install deployment object files (xilzcu102)
 cd ${THIS_DIR}
 source ${THIS_DIR}/sdk/sourceme.sh
-mkdir -p ${PULP_SDK_HOME}/install/overlay/${ov_cfg_device}/xilzcu102
-$HERO_INSTALL/bin/riscv32-unknown-elf-gcc -Wextra -Wall -Wno-unused-parameter -Wno-unused-variable -Wno-unused-function -Wundef -fdata-sections -ffunction-sections -I${PULP_SDK_INSTALL}/include/io -I${PULP_SDK_INSTALL}/include -march=rv32imcxpulpv2 -D__riscv__ -include refs/${ov_cfg_device}/xilzcu102/cl_config.h -c refs/rt_conf.c -o ${PULP_SDK_HOME}/install/overlay/${ov_cfg_device}/xilzcu102/rt_conf.o
-cp -r ${THIS_DIR}/refs/${ov_cfg_device}/xilzcu102/* ${PULP_SDK_HOME}/install/overlay/${ov_cfg_device}/xilzcu102
+mkdir -p ${install_ov_cfg_dir}/xilzcu102
+$HERO_INSTALL/bin/riscv32-unknown-elf-gcc -Wextra -Wall -Wno-unused-parameter -Wno-unused-variable -Wno-unused-function -Wundef -fdata-sections -ffunction-sections -I${PULP_SDK_INSTALL}/include/io -I${PULP_SDK_INSTALL}/include -march=rv32imcxpulpv2 -D__riscv__ -include refs/${ov_cfg_device}/xilzcu102/cl_config.h -c refs/rt_conf.c -o ${install_ov_cfg_dir}/xilzcu102/rt_conf.o
+cp -r ${THIS_DIR}/refs/${ov_cfg_device}/xilzcu102/* ${install_ov_cfg_dir}/xilzcu102
 
 # Install simulation object files (sim)
 cd ${THIS_DIR}
 source ${THIS_DIR}/sdk/sourceme.sh
-mkdir -p ${PULP_SDK_HOME}/install/overlay/${ov_cfg_device}/sim
-$HERO_INSTALL/bin/riscv32-unknown-elf-gcc -Wextra -Wall -Wno-unused-parameter -Wno-unused-variable -Wno-unused-function -Wundef -fdata-sections -ffunction-sections -I${PULP_SDK_INSTALL}/include/io -I${PULP_SDK_INSTALL}/include -march=rv32imcxpulpv2 -D__riscv__ -include refs/${ov_cfg_device}/sim/cl_config.h -c refs/rt_conf.c -o ${PULP_SDK_HOME}/install/overlay/${ov_cfg_device}/sim/rt_conf.o
-cp -r ${THIS_DIR}/refs/${ov_cfg_device}/sim/* ${PULP_SDK_HOME}/install/overlay/${ov_cfg_device}/sim
+mkdir -p ${install_ov_cfg_dir}/sim
+$HERO_INSTALL/bin/riscv32-unknown-elf-gcc -Wextra -Wall -Wno-unused-parameter -Wno-unused-variable -Wno-unused-function -Wundef -fdata-sections -ffunction-sections -I${PULP_SDK_INSTALL}/include/io -I${PULP_SDK_INSTALL}/include -march=rv32imcxpulpv2 -D__riscv__ -include refs/${ov_cfg_device}/sim/cl_config.h -c refs/rt_conf.c -o ${install_ov_cfg_dir}/sim/rt_conf.o
+cp -r ${THIS_DIR}/refs/${ov_cfg_device}/sim/* ${install_ov_cfg_dir}/sim
+
+# Create symlink to hero-urania/sim
+ln -sf ${install_ov_cfg_dir}/xilzcu102 ${install_hero_cfg_dir}/hero-urania
+ln -sf ${install_ov_cfg_dir}/sim ${install_hero_cfg_dir}/hero-sim
 
 # ------------------------------------------------------------------------------------- #
