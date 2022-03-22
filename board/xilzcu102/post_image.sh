0d187f9a46763ae6baa1ebaa566ee2d9a4c93967
Gianluca Bellocchi <gbellocchi@badile14.ee.ethz.ch>
2022-03-22 16:15:52 +0100

    update post_image script to run zcu102.sh script from correct position.

--- board/xilzcu102/post_image.sh
+++ board/xilzcu102/post_image.sh
@@ -4,7 +4,7 @@
 #ln -sf hero.dtb ${BINARIES_DIR}/system.dtb
 
 # build the petalinux zcu102 config
-cd ${BR2_EXTERNAL_HERO_PATH}/petalinux/
+cd ${BR2_EXTERNAL_HERO_PATH}/petalinux/scripts/
 ./zcu102.sh
 cp ${BR2_EXTERNAL_HERO_PATH}/petalinux/zcu102/images/linux/{bl31.bin,bl31.elf,image.ub,Image,pmufw.elf,regs.init,system.dtb,System.map.linux,u-boot.bin,zynqmp_fsbl.elf} $1
 cp ${BR2_EXTERNAL_HERO_PATH}/petalinux/zcu102/images/linux/BOOT.BIN $1/boot.bin
